a25c578b72238e8355231a7f28c8a3f0ed6a1874
t0des-dev <t0des-dev@users.noreply.github.com>
2026-06-15 10:50:04 +0100

    Inject NEXT_PUBLIC variables via next.config.mjs for reliable docker builds

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  env: {
+    NEXT_PUBLIC_API_URL: "/api",
+    NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY: "pk_test_51T8nnFRtlBRWCJjP6yN2nPbjklvryI7wbIbDTBpkgnJzlsJTCigyLy0XjfxF5XzTJIUEWAMo55Zdkx7bnMW5xlrC00nof24gmR"
+  },
   output: "standalone",
   images: {
     unoptimized: true,
